e524fb6dbb3ed6526a2fd28b7a9a351877e7265c
JAVIEL_MD <jaydenofficial76@gmail.com>
2025-04-04 10:56:39 +0300

    Add files via upload

---
+++ commandes/menu.js
@@ -0,0 +1,101 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+
+zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+ cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('EAT');
+
+// Créer une date et une heure en EAT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+╭━━━☢︎︎*☆ 𝐐𝐔𝐄𝐄𝐍 𝐗𝐌𝐃 ☆*☢︎︎━━━❍
+┃❍╭──────────────߷
+┃❍│▸  *ᴅᴀᴛᴇ*: ${date}
+┃❍│▸  *ᴛɪᴍᴇ ɴᴏᴡ*: ${temps}
+┃❍│▸  *ᴘʀᴇғɪx* : [  ${s.PREFIXE}  ]
+┃❍┃▸  *ᴍᴏᴅᴇ* :  ${mode} mode
+┃❍┃▸  *ᴘʟᴜɢɪɴs* : ${cm.length}
+┃❍│▸  *ʀᴜɴɴɪɴɢ ᴏɴ* : ${os.platform()}
+┃❍│▸  *ᴏᴡɴᴇʀ* :  ${s.OWNER_NAME}
+┃❍│▸  *ᴅᴇᴠᴇʟᴏᴘᴇʀ* : ǫᴜᴇᴇɴ ᴅᴇᴠ
+┃❍│▸  *ᴛɪᴍᴇᴢᴏɴᴇ* : ${s.TZ}
+┃❍╰───────────────߷
+╰━━━━━━━━━━━━━━━❍
+☆𝐐𝐔𝐄𝐄𝐍 𝐓𝐄𝐂𝐇 𝐁𝐎𝐓☆\n${readmore}`;
+    
+ 
+    let menuMsg=`  
+  *ǫᴜᴇᴇɴ ᴍᴅ ᴄᴏᴍᴍᴀɴᴅs*
+`;
+
+    for (const cat in coms) {
+        menuMsg += `*╭────❂* *${cat}* *❂*`;
+        for (const cmd of coms[cat]) {
+            menuMsg += `  
+*┊☆* ${cmd}`;
+        }
+        menuMsg += `
+*╰═════════════❂* \n`
+    }
+
+    menuMsg += `
+◇            ◇
+*—————☆☆☆☆—————*
+
+  ☆ǫᴜᴇᴇɴ ᴛᴇᴄʜ☆                                        
+*╰═════════════❂*
+`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "*queen*" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+});
